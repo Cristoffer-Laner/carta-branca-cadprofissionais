fba89c9dce0bcdaba2467ab6c2cd591d405f3838
Jennifer <jennifertorchelsen@gmail.com>
2023-09-17 18:39:50 -0300

    puxando os dados para a listagem

--- app/listagem/page.jsx
+++ app/listagem/page.jsx
@@ -1,4 +1,41 @@
+'use client'
+import { useEffect, useState } from "react"
+
 export default function Listagem() {
+	const [profissionais, setProfissionais] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		async function getProfissionais(){
+			const response = await fetch("http://localhost:3004/profissionais")
+			const dados = await response.json()
+			setProfissionais(dados)
+			setIsLoading(false)
+		}
+		getProfissionais()
+	}, [])
+
+	const listaProfissionais = profissionais.map(profissional => (
+		<tr key={profissional.CPF}>
+			<td>
+				<img src={profissional.imagem} alt={`Imagem do Profissional ${profissional.nome}`} width={100} />
+			</td>
+			<td>{profissional.nome}</td>
+			<td>{profissional.CPF}</td>
+			<td>{profissional.dataNasc}</td>
+			<td>{profissional.especialidade}</td>
+			<td></td>
+		</tr>
+	))
+
+	if(isLoading){
+		return(
+			<div className="container">
+				<h2 className="mt-3">Listagem de Profissionais</h2>
+				<h4>Aguarde... Carregando do Profissionais</h4>
+			</div>
+		)
+	}
     return (
 		<div className="container">
 			<h2 className="mt-3">Listagem de Filmes</h2>
@@ -14,7 +51,7 @@ export default function Listagem() {
 					</tr>
 				</thead>
 				<tbody>
-
+					{listaProfissionais}
 				</tbody>
 			</table>
 		</div>
