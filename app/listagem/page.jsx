b239d51c18afb45ebb98a9d8a773dac91614c552
Jennifer <jennifertorchelsen@gmail.com>
2023-09-17 17:53:55 -0300

    adicionado colunas para a listagem

--- app/listagem/page.jsx
+++ app/listagem/page.jsx
@@ -1,5 +1,22 @@
 export default function Listagem() {
     return (
-        <h2>Listagem de Filmes</h2>
+		<div className="container">
+			<h2 className="mt-3">Listagem de Filmes</h2>
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th>Profissional</th>
+						<th>Nome</th>
+						<th>CPF</th>
+						<th>Nascimento</th>
+						<th>Especialidade</th>
+						<th>Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				</tbody>
+			</table>
+		</div>
     )
 }
